919385acfefe047d46b4921bac2dbb9c62cfab5e
tomlingb <tomlingb@rose-hulman.edu>
2018-01-18 10:37:25 -0500

    Finished m6

--- src/m6_mutation.py
+++ src/m6_mutation.py
@@ -4,8 +4,9 @@ In this module, you mutate by CHANGING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Geoffrey Tomlinson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
 
 # ----------------------------------------------------------------------
 # STUDENTS:
@@ -129,7 +130,16 @@ def RETURN_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+
+    new_list = []
+    for k in range(len(numbers)):
+        new_list.append(numbers[k])
+    for k in range(len(numbers)):
+        if new_list[k] < 0:
+            new_list[k] = 0
+    return new_list
+
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
@@ -185,10 +195,16 @@ def MUTATE_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
-    #          Make sure that you understand it.
-    #          Then, IMPLEMENT and test THIS FUNCTION
-    #          (using the above code for testing).
+
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            numbers[k] = 0
+
+            # DONE: 3. First, READ THE ABOVE TEST CODE.
+            #          Make sure that you understand it.
+            #          Then, IMPLEMENT and test THIS FUNCTION
+            #          (using the above code for testing).
+
 
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
